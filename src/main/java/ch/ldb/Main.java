e33eabbf72422c2d96e35e7dfc8c080ef8f2cd42
mateuszmatejkokurowski <mateusz.kurowski@hftm.ch>
2025-01-14 22:12:47 +0100

    created FileInputPlugin, reading input to terminal

--- src/main/java/ch/ldb/Main.java
+++ src/main/java/ch/ldb/Main.java
@@ -2,6 +2,7 @@ package ch.ldb;
 
 import ch.ldb.core.ConfigLoader;
 import ch.ldb.core.PluginFactory;
+import ch.ldb.plugins.FileInputPlugin;
 import ch.ldb.plugins.Plugin;
 import ch.ldb.plugins.TerminalPlugin;
 
@@ -10,6 +11,7 @@ public class Main {
         // Load configuration
         String configFilePath = "src\\main\\java\\ch\\ldb\\.config.txt";
         ConfigLoader configLoader = new ConfigLoader(configFilePath);
+        FileInputPlugin fileInputPlugin = new FileInputPlugin();
 
         // Create the plugin factory
         PluginFactory pluginFactory = new PluginFactory(configLoader);
@@ -26,5 +28,12 @@ public class Main {
 
         // Start reading from the terminal
         terminalPlugin.startReadingFromTerminal();
+
+        // read the input file
+        fileInputPlugin.getOutput().subscribe(data -> {
+            System.out.println("Received: " + data);
+        });
+
+        fileInputPlugin.readFromFile("src\\main\\java\\ch\\ldb\\.input.txt");
     }
 }
